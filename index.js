f1a2fe1aa0b4ca57ed50ca8ff050d8bbd3324721
Pedro Marquez <pedro.f.marquez.soto@accenture.com>
2017-04-16 20:55:01 -0500

    Updated references to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Pedro on 3/29/2017.
+ */
+
+import APIGatewayClient from './apigateway/APIGatewayClient'
+import Utils from './apigateway/Utils'
+import SigV4Client from './apigateway/SigV4Client'
+import SimpleHttpClient from './apigateway/SimpleHttpClient'
+import {uritemplate} from './apigateway/url-template'
+
+export {APIGatewayClient, Utils, SigV4Client, SimpleHttpClient, uritemplate}
